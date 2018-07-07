19758620d7809aa8065471e5e7769ed0ac126ad9
Usoka <27248545+Usoka@users.noreply.github.com>
2018-07-08 00:32:48 +1200

    Simplify main (still only testing)

--- main.c
+++ main.c
@@ -1,11 +1,6 @@
-#include <stdio.h>
-#include "restAction.h"
+#include "discordAPI.h"
 
 int main() {
-	struct rest_action* restAction = NULL;
-	restAction = rest_action_init(restAction);
-	rest_action_make_request(restAction, "users/@me/guilds", RESTACTION_GET);
-	rest_action_cleanup(restAction);
-
+	dapi_get_guilds();
 	return 0;
 }
